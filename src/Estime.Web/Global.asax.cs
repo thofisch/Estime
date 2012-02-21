eb964af4c847d6307752265daef1d76f0184e5d1
Thomas Fischer <thofisch@gmail.com>
2012-02-21 22:44:22 +0100

    Schema builder

--- src/Estime.Web/Global.asax.cs
+++ src/Estime.Web/Global.asax.cs
@@ -61,7 +61,7 @@ namespace Estime.Web
 				})
 				.BuildConfiguration();
 
-			//new SchemaExport(configuration).Execute(true, true, false);
+			new SchemaExport(configuration).Execute(true, true, false);
 
 			SessionFactory = configuration.BuildSessionFactory();
 
